c3f31009ecefe3d1235862b09d9773c073c90803
miahsu <miahsuwork@gmail.com>
2020-08-16 15:56:41 +0800

    觀看範例修正 hw1

--- homeworks/week8/hw1/main.js
+++ homeworks/week8/hw1/main.js
@@ -1,5 +1,5 @@
+/* eslint-disable func-names */
 const URL = 'https://dvwhnbka7d.execute-api.us-east-1.amazonaws.com/default/lottery';
-const request = new XMLHttpRequest();
 const objPrize = {
   first: {
     text: '恭喜你中頭獎了！日本東京來回雙人遊！',
@@ -18,64 +18,90 @@ const objPrize = {
     elClass: 'none',
   },
 };
+const errorMessage = '系統不穩定，請再試一次';
 
 // 內容置換
 function renderPrize(obj) {
   const { text, elClass } = obj;
-  const textBlock = document.querySelector('.prize__text');
   const prizeSection = document.querySelector('.prizeDraw');
-  const descBlock = document.querySelector('.prizeDraw__desc');
-  const btnLottery = document.querySelector('.btn-prizeDraw');
+  const descBlock = document.querySelector('.lotteryDesc');
+  const resultBlock = document.querySelector('.lotteryResult');
+  const resultText = resultBlock.querySelector('.prize__text');
 
-  textBlock.innerText = text;
-  textBlock.classList.add('show');
-  prizeSection.classList.add(elClass);
   descBlock.classList.add('hide');
-  btnLottery.innerText = '再抽一次';
-}
-
-// 錯誤訊息
-function alertError() {
-  alert('系統不穩定，請再試一次');
+  resultBlock.classList.add('show');
+  resultText.innerText = text;
+  prizeSection.classList.add(elClass);
 }
 
 // 抽獎
 function lottery(resText) {
-  const prize = JSON.parse(resText).prize || null;
+  const prize = resText.prize || null;
+  let prizeText = '';
   switch (prize) {
     case 'FIRST':
-      renderPrize(objPrize.first);
+      prizeText = objPrize.first;
       break;
     case 'SECOND':
-      renderPrize(objPrize.second);
+      prizeText = objPrize.second;
       break;
     case 'THIRD':
-      renderPrize(objPrize.third);
+      prizeText = objPrize.third;
       break;
     case 'NONE':
-      renderPrize(objPrize.none);
+      prizeText = objPrize.none;
       break;
     default:
-      alertError();
+      alert(errorMessage);
+      return;
   }
+  renderPrize(prizeText);
 }
 
 // 發送 request
-request.onload = function () {
-  if (request.status >= 200 && request.status < 400) {
-    lottery(request.responseText);
-  } else {
-    alertError();
-  }
-};
-request.onerror = function () {
-  alertError();
-};
+function draw(cb) {
+  const request = new XMLHttpRequest();
+  request.open('GET', URL);
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      let data = '';
+      // 新增測試
+      try {
+        data = JSON.parse(request.responseText);
+      } catch (err) {
+        cb(errorMessage);
+        console.log(err);
+        return;
+      }
+      cb(null, data);
+    } else {
+      cb(errorMessage);
+    }
+  };
+  request.onerror = function () {
+    cb(errorMessage);
+  };
+  request.send();
+}
+
 
 // 監聽 click 事件
 window.onload = function () {
   document.querySelector('.btn-prizeDraw').addEventListener('click', () => {
-    request.open('GET', URL);
-    request.send();
+    draw((err, data) => {
+      if (err) {
+        alert(err);
+        return;
+      }
+      lottery(data);
+    });
+  });
+  document.querySelector('.resetLottery').addEventListener('click', () => {
+    const prizeSection = document.querySelector('.prizeDraw');
+    const descBlock = document.querySelector('.lotteryDesc');
+    const resultBlock = document.querySelector('.lotteryResult');
+    descBlock.classList.remove('hide');
+    resultBlock.classList.remove('show');
+    prizeSection.className = 'prizeDraw';
   });
 };
